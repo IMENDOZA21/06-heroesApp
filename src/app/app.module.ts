bf0035574a14a1b3e6130a5ef13aa6cae0144a26
IMENDOZA <imendoza@unab.edu.co>
2024-03-23 09:50:01 -0500

    ADD - Modulos Principales y enrutador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
