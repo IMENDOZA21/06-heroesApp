0bd9b2db7fa5e2be96680c29a22bcb85ba2e7aa8
IMENDOZA <imendoza@unab.edu.co>
2024-04-17 23:32:20 -0500

    Mantener la sesion del usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'heroesApp';
+
+  constructor(
+    private authService: AuthService
+  ) { }
+
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environments } from '../../../environments/enviroments';
 import { User } from '../interfaces/user.interface';
-import { Observable, retry, retryWhen, tap } from 'rxjs';
+import { Observable, catchError, delay, map, of, retry, retryWhen, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -24,13 +24,29 @@ export class AuthService {
   login(email: string, pwd: string): Observable<User> {
     return this.http.get<User>(`${this.baseUrl}/users/1`)
       .pipe(
-        tap( user => this.user = user),
-        tap( user => localStorage.setItem('token', 'eesefesfsf.2132d1d.21ddee.fdsfsf'))
+        tap(user => this.user = user),
+        tap(user => localStorage.setItem('token', 'eesefesfsf.2132d1d.21ddee.fdsfsf'))
       );
   }
 
   logout(): void {
     this.user = undefined;
     localStorage.clear();
+  }
+
+  checkAuthentication(): Observable<boolean>{
+    if (!localStorage.getItem('token')) return of(false);
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+      .pipe(
+        tap(user => this.user = user),
+        map(user => !!user),
+        catchError(err => {
+          console.error('Error al ejecutar la peticion',err);
+          return of(false);
+        })
+      );
   }
 }
